70939f9103cedd0a4635e7d372bf88b01fa49394
DevLucasOliveira <lucasoliveira.si@outlook.com>
2022-04-06 21:04:06 -0300

    study: single responsibility principle

---
+++ 01 - SOLID/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("I cried today");
+            journal.AddEntry("I ate a bug");
+            Console.WriteLine(journal);
+
+            var persistence = new Persistence();
+            var fileName = @"c:\temp\journal.txt";
+            persistence.SaveToFile(journal, fileName, true);
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo(fileName)
+                {
+                    UseShellExecute = true
+                }
+            };
+            process.Start();
+        }
+    }
+}
